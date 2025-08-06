8e06d99c02c33c3ae5ddef2cf248cb44dc33b4d3
Danilo Barrote <danilo.barrote@hotmail.com>
2025-08-05 23:24:49 -0300

    ajeitando print consulta 2 e 3

--- database/queries/subconsulta_linha.py
+++ database/queries/subconsulta_linha.py
@@ -30,6 +30,6 @@ def subconsulta_linha():
         cur.execute(query)
         resultados = cur.fetchall()
         for r in resultados:
-            print(r)
+            print(f"CPF: {r[0]}, CRM: {r[1]}")
 
     conn.close()
